7a9f563ba775fe59a25798da6d21466b1cdc4c28
Santiago Enciso <whereispolaris@gmail.com>
2019-09-08 13:24:19 -0500

    Created Post Model

--- models/post.js
+++ models/post.js
@@ -1,5 +1,25 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   // Add code here to create a Post model
-  // This model needs a title, a body, and a category
+  const Post = sequelize.define("Post", {
+    // This model needs a title, a body, and a category
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    category: {
+      type: DataTypes.STRING,
+      defaultValue: "Personal"
+    },
+  });
+
   // Don't forget to 'return' the post after defining
+  return Post;
 };
