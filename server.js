e876fb3a5c8a88e6679066524035675092f1bffa
Santiago Enciso <whereispolaris@gmail.com>
2019-09-08 14:50:54 -0500

    Declared static directory and added API/HTML routes to server.js file.

--- server.js
+++ server.js
@@ -18,9 +18,18 @@ var db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
