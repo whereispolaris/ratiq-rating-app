67f1c006a54415e4fdd46737680a0a15310f8743
Santiago Enciso <whereispolaris@gmail.com>
2019-09-08 14:20:14 -0500

    Added sequelize code to api routes to get all posts, find by id or category, and create new ones.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,24 +15,50 @@ module.exports = function (app) {
     // GET route for getting all of the posts
     app.get("/api/posts", function (req, res) {
         // Add sequelize code to find all posts, and return them to the user with res.json
+        db.Post.findAll({})
+            .then(dbPost => {
+                res.json(dbPost);
+            });
     });
 
     // Get route for returning posts of a specific category
     app.get("/api/posts/category/:category", function (req, res) {
         // Add sequelize code to find all posts where the category is equal to req.params.category,
         // return the result to the user with res.json
+        db.findAll({
+            where: {
+                category: req.params.category
+            }
+        }).then(dbPost => {
+            res.json(dbPost);
+        });
     });
 
     // Get route for retrieving a single post
     app.get("/api/posts/:id", function (req, res) {
         // Add sequelize code to find a single post where the id is equal to req.params.id,
         // return the result to the user with res.json
+        db.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(dbPost => {
+            res.json(dbPost);
+        });
     });
 
     // POST route for saving a new post
     app.post("/api/posts", function (req, res) {
         // Add sequelize code for creating a post using req.body,
         // then return the result using res.json
+        db.create({
+            title: req.body.title,
+            body: req.body.body,
+            category: req.body.category
+        }).then(dbPost => {
+            res.json(dbPost);
+        })
+
     });
 
     // DELETE route for deleting posts
