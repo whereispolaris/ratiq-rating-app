0aae14a3f9168d4eb2cd9cb87551fb8d8faf4010
Santiago Enciso <whereispolaris@gmail.com>
2019-09-16 18:09:13 -0500

    Updated schema file and added Heroku Config Vars.

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS blogger;
--- Creates the "blogger" database --
-CREATE DATABASE blogger;
+-- Drops the ratiq_users if it exists currently --
+DROP DATABASE IF EXISTS ratiq_users;
+-- Creates the "ratiq_users" database --
+CREATE DATABASE ratiq_users;
