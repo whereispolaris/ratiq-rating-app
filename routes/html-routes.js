d3e5edd35ce856ad6834711ad485a1397fcdf9bd
Santiago Enciso <whereispolaris@gmail.com>
2019-09-16 21:10:33 -0500

    Updated credentials in config.json.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,17 +10,17 @@ module.exports = function (app) {
 
     // index route loads Homepage
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join("../public/index.html"));
     });
 
     // Route to the cms page
     app.get("/cms", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/cms.html"));
+        res.sendFile(path.join("../public/cms.html"));
     });
 
     // blog route loads blog.html
     app.get("/profile", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/profile.html"));
+        res.sendFile(path.join("../public/profile.html"));
     });
 
 };
